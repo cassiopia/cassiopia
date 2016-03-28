382c0067ac31b356babea1a166064394cbd12677
Natalia Rudakova <natalia.rudakova@hiqo-solutions.com>
2016-03-28 18:30:32 +0300

    Вживила шаблон  AdminLTE для админки

--- advanced/backend/config/main.php
+++ advanced/backend/config/main.php
@@ -61,6 +61,11 @@ return [
                 ],
                 // ...
             ],
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ],
+            ],
         ],
 
     ],
